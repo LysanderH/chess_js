1591b1ce809475a3c5d5f378b3c28d68f742e3d3
Lysander Hans <lysander.hans@hotmail.com>
2019-06-16 21:08:19 +0200

    Added the calculatePosition method

---
+++ src/js/king_white.js
@@ -0,0 +1,27 @@
+export const king = {
+    game: null,
+    coordinates: {
+        frameX: 24,
+        frameY: 24,
+        frameW: 165,
+        frameH: 168,
+        canvasX: 0,
+        canvasY: 0,
+        canvasW: 0,
+        canvasH: 0
+    },
+    position: {
+        col: 4,
+        row: 7
+    },
+    init(game) {
+        this.game = game;
+        this.coordinates.canvasW = this.game.c.canvas.height / 8 - 10;
+        this.coordinates.canvasH = this.game.c.canvas.height / 8 - 10;
+        this.coordinates.canvasX = this.game.c.canvas.height / 8 * this.position.col + this.game.canvas.height / 16 - this.coordinates.canvasW / 2;
+        this.coordinates.canvasY = this.game.c.canvas.height / 8 * this.position.row + this.game.canvas.height / 16 - this.coordinates.canvasH / 2;
+    },
+    draw() {
+        this.game.renderSpriteFrame(this.coordinates);
+    }
+};
